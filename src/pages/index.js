8a500fd1e654727e4ed9358e357f94b83e644515
Naseem <1610915@brunel.ac.uk>
2021-12-25 19:03:12 +0000

    home page created with css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './index.css';
 
 const Home = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.location.href='./about';
+  };
   return (
-    <div>
-      <p>home</p>
+    <div className="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+      <h1 className="text-white" id="header-text1">Hello,<br /> I am Naseem.</h1>
+      <div className="content text-center">
+        <h1 className="text-white" id="header-text2">
+            Computer Science (Network Computing) Graduate <br />
+            Achieved 2.1 with Honours <br />
+            A technology enthusiast who loves to learn and adapt. <br />
+        </h1>
+        <button id="button" className="btn btn-primary btn-md learn-more" type="button" onClick={handleClick}>Learn more</button>
+      </div>
     </div>
   );
 };
